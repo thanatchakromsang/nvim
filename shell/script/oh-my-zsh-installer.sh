aed1114ce6a01f095d4f5c22e79a3a7c8f3a3a66
thanatchakromsang <thanatchakromsang@gmail.com>
2018-01-12 17:21:14 +0700

    change zsh installer method

--- shell/script/oh-my-zsh-installer.sh
+++ shell/script/oh-my-zsh-installer.sh
@@ -5,7 +5,8 @@ echo -n "answer 'y'es or 'n'o : "
 read REPLY
 if [[ "$REPLY" == "y" ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins
+    cd ~/.oh-my-zsh/custom/plugins && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+    source ~/.zshrc
     echo "setup oh my zsh complete"
     echo
 else
