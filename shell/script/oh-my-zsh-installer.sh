5b177a62a14e2869cd3bd7b76e80ffc4302fa089
thanatchakromsang <thanatchakromsang@gmail.com>
2018-01-24 11:01:27 +0700

    update script for new symlink include .ssh

--- shell/script/oh-my-zsh-installer.sh
+++ shell/script/oh-my-zsh-installer.sh
@@ -6,6 +6,7 @@ read REPLY
 if [[ "$REPLY" == "y" ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     cd ~/.oh-my-zsh/custom/plugins && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
     source ~/.zshrc
     echo "setup oh my zsh complete"
     echo
