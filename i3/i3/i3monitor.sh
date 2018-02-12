077bcc085f1fc6e68d850cc8527d42a9e8d3d9d1
thanatchakromsang <thanatchakromsang@gmail.com>
2018-02-12 08:12:36 +0700

    forbid fn use, add multi monitor support for i3

---
+++ i3/i3/i3monitor.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+INTERN="eDP1"
+
+case "$1" in
+    hdmi)
+        EXTERN="HDMI1"
+        EXTERN2="HDMI2"
+        ;;
+    *)
+esac
+
+case "$2" in
+    intern)
+        xrandr --output $EXTERN --off --output $EXTERN2 --off --output $INTERN --auto
+        sh $HOME/.config/i3/wallpaper single
+        ;;
+    extern)
+        xrandr --output $EXTERN --auto --output $EXTERN2 --off --output $INTERN --off
+        sh $HOME/.config/i3/wallpaper single
+        ;;
+    clone)
+        CLONERES=`xrandr --query | awk '/^ *[0-9]*x[0-9]*/{ print $1 }' | sort -n | uniq -d | tail -1`
+        xrandr --output $INTERN --mode $CLONERES --pos 0x0 --output $EXTERN --mode $CLONERES --pos 0x0 --output $EXTERN2 --off
+        sh $HOME/.config/i3/wallpaper single
+        ;;
+    extend)
+        xrandr --output $INTERN --auto --output $EXTERN --auto --right-of $INTERN --output $EXTERN2 --off
+        sh /home/mike/.scripts/bin/wallpaper dual
+        ;;
+    *)
+        usage
+        ;;
+esac
+
+exit 0
---
+++ i3/i3/wallpaper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case "$1" in
+    single)
+        ~/.fehbg &
+        ;;
+    dual)
+        ~/.fehbg &
+        ;;
+    *)
+        ;;
+esac
