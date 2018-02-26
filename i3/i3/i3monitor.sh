e8c9aa8f60e8f522a7ad430094f703e70d06ba44
thanatchakromsang <thanatchakromsang@gmail.com>
2018-02-26 15:28:39 +0700

    updated dpi to 1.25, make multimonitor better

--- i3/i3/i3monitor.sh
+++ i3/i3/i3monitor.sh
@@ -14,19 +14,23 @@ case "$2" in
     intern)
         xrandr --output $EXTERN --off --output $EXTERN2 --off --output $INTERN --auto
         sh $HOME/.config/i3/wallpaper single
+        i3-msg restart
         ;;
     extern)
         xrandr --output $EXTERN --auto --output $EXTERN2 --off --output $INTERN --off
         sh $HOME/.config/i3/wallpaper single
+        i3-msg restart
         ;;
     clone)
         CLONERES=`xrandr --query | awk '/^ *[0-9]*x[0-9]*/{ print $1 }' | sort -n | uniq -d | tail -1`
         xrandr --output $INTERN --mode $CLONERES --pos 0x0 --output $EXTERN --mode $CLONERES --pos 0x0 --output $EXTERN2 --off
         sh $HOME/.config/i3/wallpaper single
+        i3-msg restart
         ;;
     extend)
         xrandr --output $INTERN --auto --output $EXTERN --auto --right-of $INTERN --output $EXTERN2 --off
         sh /home/mike/.scripts/bin/wallpaper dual
+        i3-msg restart
         ;;
     *)
         usage
