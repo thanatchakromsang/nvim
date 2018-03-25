d622ab28536b121266f7722e781b3cec1884a998
thanatchakromsang <thanatchakromsang@gmail.com>
2018-03-25 17:47:18 +0700

    Add touchpad toggle for intern/extern mode

--- i3/i3/i3monitor.sh
+++ i3/i3/i3monitor.sh
@@ -15,11 +15,13 @@ case "$2" in
         xrandr --output $EXTERN --off --output $EXTERN2 --off --output $INTERN --auto
         sh $HOME/.config/i3/wallpaper single
         i3-msg restart
+        xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 0
         ;;
     extern)
         xrandr --output $EXTERN --auto --output $EXTERN2 --off --output $INTERN --off
         sh $HOME/.config/i3/wallpaper single
         i3-msg restart
+        xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 0
         ;;
     clone)
         CLONERES=`xrandr --query | awk '/^ *[0-9]*x[0-9]*/{ print $1 }' | sort -n | uniq -d | tail -1`
