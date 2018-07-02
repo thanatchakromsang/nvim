03f5f8a740a488b493c7ed348244aedb1d55f87a
thanatchakromsang <thanatchakromsang@gmail.com>
2018-07-02 09:01:11 +0700

    update polybar taskwarrior script :muscle:

--- i3/polybar/task_polybar.sh
+++ i3/polybar/task_polybar.sh
@@ -6,21 +6,16 @@ most_urgent_id=`task rc.verbose: rc.report.next.columns:id rc.report.next.labels
 
 most_urgent_due=`task rc.verbose: rc.report.next.columns:due.relative rc.report.next.labels:1 limit:1 next`
 
-active_task_age=`task rc.verbose: rc.report.next.columns:start.age rc.report.next.labels:1 limit:1 next`
+active_task_age=`task rc.verbose: rc.report.active.columns:start.age rc.report.active.labels: active limit:1`
 
-active_task_toggle=`task rc.verbose: rc.report.next.columns:start.active rc.report.next.labels:1 limit:1 next`
+active_task_check=`task rc.verbose: rc.report.active.columns:start.active rc.report.active.labels: active limit:1`
 
-echo "$most_urgent_id" > /tmp/tw_polybar_id
-echo -n "$most_urgent_desc"
+current_active_task=`task rc.verbose: rc.report.active.columns:description rc.report.active.labels: active limit:1`
 
-if [ ! "$most_urgent_due" = "" ]
-  then
-    echo -n " due: $most_urgent_due"
-fi
-
-if [ "$active_task_toggle" = "*" ]
-  then
-    echo -n " started: $active_task_age"
+if [ "$active_task_check" = "*" ]; then
+    echo -n "$current_active_task | started: $active_task_age"
+else
+    echo -n " $most_urgent_desc | due: $most_urgent_due"
 fi
 
 
