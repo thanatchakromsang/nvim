bbabc384f3afb33ed538844dcc2366b8c7a60d32
thanatchakromsang <thanatchakromsang@gmail.com>
2018-07-02 09:04:13 +0700

    clean up i3monitor script :muscle:

--- i3/i3/i3monitor.sh
+++ i3/i3/i3monitor.sh
@@ -15,13 +15,11 @@ case "$2" in
         xrandr --output $EXTERN --off --output $EXTERN2 --off --output $INTERN --auto
         # sh ~/.dotfiles/i3/i3/random_wallpaper single
         i3-msg restart
-        xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 1
         ;;
     extern)
         xrandr --output $EXTERN --auto --output $EXTERN2 --off --output $INTERN --off
         # sh ~/.dotfiles/i3/i3/random_wallpaper.sh single
         i3-msg restart
-        xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 0
         ;;
     clone)
         CLONERES=`xrandr --query | awk '/^ *[0-9]*x[0-9]*/{ print $1 }' | sort -n | uniq -d | tail -1`
@@ -33,7 +31,6 @@ case "$2" in
         xrandr --output $INTERN --auto --output $EXTERN --auto --above $INTERN --output $EXTERN2 --off
         # sh ~/.dotfiles/i3/i3/random_wallpaper.sh single
         i3-msg restart
-        xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 1
         ;;
     *)
         usage
