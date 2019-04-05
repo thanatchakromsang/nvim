ccaa537bedd98beca93907d724eb651f49555a9f
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2019-04-05 07:13:42 +0700

    🔧 [Pacman] Update arch installation script

--- pacman.sh
+++ pacman.sh
@@ -14,20 +14,17 @@ declare -a pkgs=("curl"
                  "zsh"
                  "scrot"
                  "tig"
-                 "bat")
+                 "bat"
+                 "z"
+                 )
 
 for i in "${pkgs[@]}"
 do
    sudo pacman -S "$i"
 done
 
-for i in "${pip[@]}"
-do
-   pip install -S "$i"
-done
-
 # Graphic packages
-declare -a g_pkgs=("dunst" "compton" "rofi" "zathura")
+declare -a g_pkgs=("dunst" "compton" "rofi" "zathura", "kubectl", "kubectx")
 
 if [[ $(program_is_installed X) -eq 1 ]]; then
   for i in "${g_pkgs[@]}"
@@ -37,9 +34,15 @@ if [[ $(program_is_installed X) -eq 1 ]]; then
 
   for i in "${g_yurt[@]}"
   do
-    yaourt -S "$i" --noconfirm
+    yay -S --noconfirm "$i"
   done
 fi
 
+# pip
 declare -a pip=("pipenv" "jedi")
+
+for i in "${pip[@]}"
+do
+   pip install -S "$i"
+done
 
