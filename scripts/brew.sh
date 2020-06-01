b4c1da04311c87981b5cf1e29ad3de8fe7452571
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2020-06-01 14:09:43 +0700

    Update brew packages

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -31,7 +31,7 @@ brew install screen
 brew install fontconfig
 
 # Install other useful binaries.
-declare -a bin=("git" "exa" "rg" "stow" "tig" "bat" "fzf" "lastpass-cli")
+declare -a bin=("git" "exa" "rg" "stow" "tig" "bat" "fzf" "lastpass-cli" "yq" "jq")
 
 for i in "${bin[@]}"
 do
