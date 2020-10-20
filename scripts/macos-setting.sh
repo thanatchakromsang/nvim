ef815615510c9e3d1bc5af4b6fe857aec76dd462
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2020-10-20 17:15:03 +0700

    Add macos hotkey shortcut and custom linux shortcut

---
+++ scripts/macos-setting.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+defaults write -g NSUserKeyEquivalents '{
+    Copy = "^c";
+    Cut = "^x";
+    Paste = "^v";
+    Redo = "^$z";
+    Undo = "^z";
+
+    "Actual Size" = "^=";
+    Back = "^\U2190";
+    "Bookmark This Tab..." = "^d";
+    "Close Tab" = "^w";
+    "Close Window" = "^q";
+    "Find Next" = "^g";
+    "Find Previous" = "^$g";
+    "Find..." = "^f";
+    Forward = "^\U2192";
+    Minimize = "@~^$m";
+    "New Incognito Window" = "^$n";
+    "New Tab" = "^t";
+    "New Window" = "^n";
+    "Open File..." = "^o";
+    "Open Location..." = "^l";
+    "Paste and Match Style" = "^$v";
+    "Print..." = "^p";
+    "Reload This Page" = "^r";
+    "Reopen Closed Tab" = "^$t";
+    "Select All" = "^a";
+    "Zoom In" = "^+";
+    "Zoom Out" = "^-";
+
+    Settings = "@~^$$";
+    "Preferences..." = "@~^$p";
+    "New OS window" = "@~^$l";
+    "Hide Google Chrome" = "@~^$l";
+    "Hide kitty" = "@~^$l";
+}'
