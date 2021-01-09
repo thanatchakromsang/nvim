11769cbc16e38e27c16d7536163a75b2f26469b0
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-01-09 12:32:34 +0700

    Dotfiles tidyup

--- @linux/.config/polybar/vpn-openvpn-status.sh
+++ @linux/.config/polybar/vpn-openvpn-status.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Get VPN Name
-printf " : " && (pgrep -a openvpn$ | head -n 1 | awk '{print $4}' | cut -d '.' -f 1 | cut -c 1- && echo off) | head -n 1
+printf " : " && (pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1 && echo down) | head -n 1
