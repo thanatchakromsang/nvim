c06b51187f1fb6ea067df6cfb56d836bc3fa612d
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-05-12 15:16:37 +0700

    feat(npm): add global prettier

--- scripts/lsp.sh
+++ scripts/lsp.sh
@@ -10,6 +10,6 @@ npm install -g typescript typescript-language-server
 npm install -g vscode-json-languageserver
 npm install -g vim-language-server
 npm install -g yaml-language-server
-# npm install -g prettier
+npm install -g prettier
 npm install -g eslint_d
 npm install -g markdownlint-cli --save-dev
