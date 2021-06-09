78f4a96f48ef25966c930093fb83541957c5d80f
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-06-09 23:33:20 +0700

    fix: add oh-my-ssh script

--- scripts/oh-my-zsh.sh
+++ scripts/oh-my-zsh.sh
@@ -1,5 +1,7 @@
-chsh -s $(which zsh)
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+chsh -s $(which zsh)
+
