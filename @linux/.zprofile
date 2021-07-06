fa03d91f6710c699ad7a7b2b12daa0c02f0ff2c0
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2021-07-06 16:27:06 +0700

    feat: add ssh-agent

--- @linux/.zprofile
+++ @linux/.zprofile
@@ -1,3 +1,4 @@
+eval $(ssh-agent)
 export PATH="$HOME/.dotfiles/scripts/bin:$PATH"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 export XDG_CURRENT_DESKTOP=sway
--- scripts/archlinux/systemctl.sh
+++ scripts/archlinux/systemctl.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-sudo tee /etc/systemd/system/resume-fix-pulseaudio@.service >/dev/null <<EOF
-[Unit]
-Description=Fix PulseAudio after resume from suspend
-After=suspend.target
-
-[Service]
-User=%I
-Type=oneshot
-Environment="XDG_RUNTIME_DIR=/run/user/$UID"
-ExecStart=/usr/bin/pasuspender /bin/true
-
-[Install]
-WantedBy=suspend.target
-EOF
-
 sudo systemctl enable --now bluetooth.service
 sudo systemctl enable --now NetworkManager.service
 sudo systemctl enable --now docker.service
-sudo systemctl enable --now resume-fix-pulseaudio@$USER.service
 
 sudo systemctl --system daemon-reload
+
